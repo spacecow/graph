19bd301b80bdbcb4b5f8a60754ef26a77aee9cb8
Johan Sveholm <jsveholm@gmail.com>
2015-05-17 02:45:58 +0900

    only define unless defined in controller specs

--- spec/controllers/universes_controller_spec.rb
+++ spec/controllers/universes_controller_spec.rb
@@ -4,24 +4,27 @@ describe "UniversesController" do
   let(:controller){ UniversesController.new }
 
   before do
-    module UniverseRunners; end
+    module UniverseRunners; end unless defined?(UniverseRunners)
     module ActionController
       class Base
         def self.protect_from_forgery s; end
       end
-    end
+    end unless defined?(ActionController)
     require File.expand_path '../../../app/controllers/application_controller', __FILE__
     require File.expand_path '../../../app/controllers/universes_controller', __FILE__
+    @params_defined = false
   end
 
   describe "#create" do
     before do
-      module UniverseRunners; class Create; end end
+      module UniverseRunners
+        class Create; end
+      end unless defined?(UniverseRunners::Create)
       on = double :on
       expect(controller).to receive(:run).with(UniverseRunners::Create, :universe){ :universe }
       controller.class.send(:define_method, :params) do
-        {universe: :universe}
-      end
+      end && @params_defined = true unless controller.class.instance_methods(false).include?(:params)
+      expect(controller).to receive(:params){ {universe: :universe} }
       controller.create
     end
 
@@ -30,7 +33,9 @@ describe "UniversesController" do
 
   describe "#new" do
     before do
-      module UniverseRunners; class New; end end
+      module UniverseRunners
+        class New; end
+      end unless defined?(UniverseRunners::New)
       expect(controller).to receive(:run).with(UniverseRunners::New){ :universe }
       controller.new
     end
@@ -43,11 +48,13 @@ describe "UniversesController" do
 
   describe "#index" do
     before do
-      module UniverseRunners; class Index; end end
+      module UniverseRunners
+        class Index; end
+      end unless defined?(UniverseRunners::Index)
       expect(controller).to receive(:run).with(UniverseRunners::Index){ :universes }
       controller.class.send(:define_method, :params) do
-        {id: :id}
-      end
+      end && @params_defined = true unless controller.class.instance_methods(false).include?(:params)
+      expect(controller).to receive(:params).at_least(1){ {id: :id} }
       controller.index   
     end
 
@@ -60,6 +67,10 @@ describe "UniversesController" do
       subject{ controller.instance_variable_get(:@current_universe) }
       it{ is_expected.to be :id }
     end
+  end
+
+  after do
+    controller.class.send(:remove_method, :params) if @params_defined
   end
 
 end
