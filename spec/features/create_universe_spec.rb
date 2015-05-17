68c4a9cfbd2a083e5e257f031430d730bfbb18ed
Johan Sveholm <jsveholm@gmail.com>
2015-05-17 19:02:24 +0900

    less individual tests in create feature spec

--- spec/features/create_universe_spec.rb
+++ spec/features/create_universe_spec.rb
@@ -12,15 +12,6 @@ describe 'Universe create' do
         fill_in 'Title', with:'Wheel of Time'
         click_on 'Create'
         expect(current_path).to eq universes_path
-        delete :universes
-      end
-    end
-
-    it "" do
-      VCR.use_cassette('create_universe') do
-        visit new_universe_path 
-        fill_in 'Title', with:'Wheel of Time'
-        click_on 'Create'
         is_expected.to have_content 'Wheel of Time'
         delete :universes
       end
