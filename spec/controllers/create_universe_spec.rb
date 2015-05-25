03ac077ceac237c668e19c2c731dcca709dee79e
Johan Sveholm <jsveholm@gmail.com>
2015-05-25 14:05:23 +0900

    stub params instead of define and remove it in spec

--- spec/controllers/create_universe_spec.rb
+++ spec/controllers/create_universe_spec.rb
@@ -10,19 +10,13 @@ describe "UniversesController#create" do
 
   describe "#create" do
     before do
-      on = double :on
-      expect(controller).to receive(:run).with(UniverseRunners::Create, :universe){ :universe }
-      controller.class.send(:define_method, :params) do
-      end && @params_defined = true unless controller.class.instance_methods(false).include?(:params)
+      expect(controller).to receive(:run).with(UniverseRunners::Create, :universe)
+      def controller.params; end
       expect(controller).to receive(:params){ {universe: :universe} }
       controller.create
     end
 
     it{ subject }
-  end
-
-  after do
-    controller.class.send(:remove_method, :params) if @params_defined
   end
 
 end
