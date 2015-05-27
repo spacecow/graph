9ac7ba7e3f2a12a89438cf85f1dde07c7af810fb
Johan Sveholm <jsveholm@gmail.com>
2015-05-27 10:25:02 +0900

    spec preparation for creation of failing article

--- spec/features/create_article_spec.rb
+++ spec/features/create_article_spec.rb
@@ -5,7 +5,28 @@ describe "Create article" do
   
   context "creation successful" do
     it "" do
-      VCR.use_cassette('create_article') do
+      VCR.use_cassette('create_article_successfully') do
+        begin 
+          universe = create :universe, title:'The Malazan Empire'
+          visit universes_path
+          click_link 'The Malazan Empire'
+          visit new_article_path
+          fill_in 'Name', with:'Kelsier'
+          select 'Character', from:'Type'
+          click_on 'Create'
+          expect(current_path).to eq universe_path(universe.id)
+          expect(page).to have_content 'Kelsier' 
+        ensure
+          delete :articles
+          delete :universes
+        end
+      end
+    end
+  end
+
+  context "creation failure" do
+    it "" do
+      VCR.use_cassette('create_article_with_failures') do
         begin 
           universe = create :universe, title:'The Malazan Empire'
           visit universes_path
