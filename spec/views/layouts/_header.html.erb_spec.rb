2e7c11a449bdf9275cc3c9b3e7f3e16d39a860b4
Johan Sveholm <jsveholm@gmail.com>
2015-08-20 21:34:33 +0200

    list articles in the header

--- spec/views/layouts/_header.html.erb_spec.rb
+++ spec/views/layouts/_header.html.erb_spec.rb
@@ -6,20 +6,33 @@ describe 'layouts/_layouts.html.erb' do
   let(:rendering){ erb.result local_bindings }
   let(:erb){ ERB.new file }
   let(:file){ File.read filepath }
-  let(:local_bindings){ erb_bindings.instance_eval{binding} }
-  let(:erb_bindings){ ErbBinding.new locals }
+  let(:local_bindings){ bind.instance_eval{binding} }
+  let(:bind){ ErbBinding.new locals }
 
   let(:filepath){ './app/views/layouts/_header.html.erb' }
   let(:locals){ {} }
 
-  subject(:div){ Capybara.string(rendering).find 'div#header a' }
+  subject(:div){ Capybara.string(rendering).find 'div#header' }
 
   before do
-    def erb_bindings.universes_path; end
-    expect(erb_bindings).to receive(:universes_path){ "path" }
+    def bind.universes_path; end
+    def bind.universe_path id; end
+    def bind.current_universe_id; end
+    expect(bind).to receive(:universes_path){ "universe_path" }
+    expect(bind).to receive(:universe_path).with(:id){ "articles_path" }
+    expect(bind).to receive(:current_universe_id).at_least(1){ :id }
   end
 
-  its(:text){ is_expected.to eq 'Universes' }
-  its([:href]){ is_expected.to eq 'path' }
+  describe "Universes link" do
+    subject{ div.all('a').first }
+    its(:text){ is_expected.to eq 'Universes' }
+    its([:href]){ is_expected.to eq 'universe_path' }
+  end
+
+  describe "Articles link" do
+    subject{ div.all('a').last }
+    its(:text){ is_expected.to eq 'Articles' }
+    its([:href]){ is_expected.to eq 'articles_path' }
+  end
 
 end 
