92edd270f07a759de462ab828cd4484bddeb0c29
Johan Sveholm <jsveholm@gmail.com>
2015-12-16 08:27:06 +0100

    participation returns association in response

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -2,8 +2,11 @@ class Participation
   include ActiveModel::Model
 
   attr_reader :participant
-  attr_writer :id
-  attr_accessor :article_id, :event_id
+  attr_writer :id, :event_id
+  attr_accessor :article_id
+
+  def event= params; @event = Event.new params end
+  def event_id; @event_id || @event.id end
 
   def gender; participant.gender end
 
