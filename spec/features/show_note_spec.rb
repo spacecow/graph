8cfbd77e80039b081699db97e7d014871f48a46e
Johan Sveholm <johan.sveholm@hemnet.se>
2016-09-06 15:55:30 +0200

    delete note tag

--- spec/features/show_note_spec.rb
+++ spec/features/show_note_spec.rb
@@ -33,6 +33,28 @@ describe 'Show note' do
     end
   end
 
+  it "Delete note tag" do
+    VCR.use_cassette('delete_note_tag') do
+      begin
+        universe = create :universe, title:"Game of Thrones"
+        note = tcreate :note
+        tcreate :tag, universe_id:universe.id, title:'Hodor',
+          tagable_id:note.id, tagable_type:'Note'
+        visit universes_path
+        click_link "Game of Thrones"
+        visit note_path note.id
+        expect(page).to have_content 'Hodor'
+        click_link 'Delete'
+        expect(current_path).to eq note_path(note.id)
+        expect(page).not_to have_content 'Hodor'
+      ensure
+        tdelete :tags
+        delete :notes
+        tdelete :universes
+      end
+    end
+  end
+
   pending "navigate to a tag page"
 
   it "navigate to a reference page" do
